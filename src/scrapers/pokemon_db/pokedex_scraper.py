3c1c8656c41eca72ae0f345cad7e20108825229b
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2021-12-08 11:11:16 -0500

    Add : directory for modules related to scraping pokemondb website

---
+++ src/scrapers/pokemon_db/pokedex_scraper.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+from bs4 import BeautifulSoup
+from bs4.element import ResultSet
+from ..scraper import Scraper
+import requests
+
+
+class PokemonDBTotalPokedexScraper(Scraper):
+    def __init__(self, url: str):
+        super().__init__()
+        self.url = url
+        self.page_source = None
+
+    def make_request(self) -> PokemonDBTotalPokedexScraper:
+        r = requests.get(url=self.url)
+        self.set_page_source(r.text)
+        return self
+
+    def set_page_source(self, raw_html: str) -> None:
+        self.page_source = BeautifulSoup(raw_html, "html.parser")
+
+    def get_page_source(self):
+        return self.page_source
+
+    def get_pokedex_table(self) -> ResultSet:
+        return self.get_page_source().table.tbody.find_all("tr")
