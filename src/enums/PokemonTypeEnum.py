830d41bb45dc0c61491de167a9ba2d98bdf7a5fe
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2021-12-21 22:41:51 -0500

    Update: enum to use string values

--- src/enums/PokemonTypeEnum.py
+++ src/enums/PokemonTypeEnum.py
@@ -2,21 +2,21 @@ import enum
 
 
 class PokemonTypeEnum(enum.Enum):
-    WATER = 1
-    FIRE = 2
-    GRASS = 3
-    ELECTRIC = 4
-    PSYCHIC = 5
-    ICE = 6
-    DRAGON = 7
-    DARK = 8
-    FAIRY = 9
-    NORMAL = 10
-    FIGHTING = 11
-    FLYING = 12
-    POISON = 13
-    GROUND = 14
-    ROCK = 15
-    BUG = 16
-    GHOST = 17
-    STEEL = 18
+    WATER = "Water"
+    FIRE = "Fire"
+    GRASS = "Grass"
+    ELECTRIC = "Electric"
+    PSYCHIC = "Psychic"
+    ICE = "Ice"
+    DRAGON = "Dragon"
+    DARK = "Dark"
+    FAIRY = "Fairy"
+    NORMAL = "Normal"
+    FIGHTING = "Fighting"
+    FLYING = "Flying"
+    POISON = "Poison"
+    GROUND = "Ground"
+    ROCK = "Rock"
+    BUG = "Bug"
+    GHOST = "Ghost"
+    STEEL = "Steel"
